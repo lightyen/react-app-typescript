754ae113d54636a87218da62aa0be2b8c76b9186
lightyen <lightyen0123@gmail.com>
2019-05-17 22:38:23 +0800

    update

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,37 +1,37 @@
 import React from "react"
 import { createHashHistory } from "history"
-import AppRouter from "./AppRouter"
 import { ConnectedRouter } from "connected-react-router"
 
-// Store
+import "./scss/style.scss"
+import "bootstrap"
+
+import AppRouter from "./AppRouter"
+
+// store
 import { configureStore } from "~/store"
 import { Provider } from "~/components/i18n"
 
-import "bootstrap"
-import "bootstrap/scss/bootstrap.scss"
-import "./scss/App.scss"
-
+// locale
 import { setLocale } from "~/store/i18n"
 
 export default function App() {
-    const [appStore, setAppStore] = React.useState(null)
+    const [store, setStore] = React.useState(null)
     React.useEffect(() => {
         document.title = "react-app-typescript"
-    }, [])
+    })
 
     const history = createHashHistory()
 
     React.useEffect(() => {
-        ;(async () => {
-            const store = configureStore(history)
-            const locale = "en-US" || navigator.languages[0]
-            await setLocale(locale)(store.dispatch, null, null)
-            setAppStore(store)
-        })()
+        const store = configureStore(history)
+        const locale = "en-US" || navigator.languages[0]
+        setLocale(locale)(store.dispatch, null, null).then(() => {
+            setStore(store)
+        })
     }, [])
 
-    return appStore ? (
-        <Provider store={appStore}>
+    return store ? (
+        <Provider store={store}>
             <ConnectedRouter history={history}>
                 <AppRouter />
             </ConnectedRouter>
