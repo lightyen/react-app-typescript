6d0bab1db10c1d480346bf17ae081594f57e6a19
lightyen <lightyen0123@gmail.com>
2019-05-28 09:24:34 +0800

    jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,6 @@
 const { defaults: tsjPreset } = require("ts-jest/presets")
 const { pathsToModuleNameMapper } = require("ts-jest/utils")
-const path = require("path")
 const tsconfig = require("./tsconfig.test.json")
-
-function getMapper() {
-  const arr = Object.entries(pathsToModuleNameMapper(tsconfig.compilerOptions.paths)).map(e => [
-    e[0],
-    path.resolve(e[1]),
-  ])
-  const mapper = {}
-  for (let i = 0; i < arr.length; i++) {
-    mapper[arr[i][0]] = arr[i][1]
-  }
-  return mapper
-}
 
 module.exports = {
   preset: "ts-jest",
@@ -24,7 +11,7 @@ module.exports = {
   },
   moduleNameMapper: {
     "\\.(css|scss)$": "<rootDir>/src/__mocks__/styleMock.js",
-    ...getMapper(),
+    ...pathsToModuleNameMapper(tsconfig.compilerOptions.paths, { prefix: "<rootDir>" }),
   },
   globals: {
     "ts-jest": {
