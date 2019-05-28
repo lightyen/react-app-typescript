2f759fb306be9a5aec6d020d733b0da2ff940766
lightyen <lightyen0123@gmail.com>
2019-05-28 11:38:10 +0800

    add mocks

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,9 @@ module.exports = {
     ...tsjPreset.transform,
   },
   moduleNameMapper: {
-    "\\.(css|scss)$": "<rootDir>/src/__mocks__/styleMock.js",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/src/__mocks__/fileMock.js",
+    "\\.(css|less|scss)$": "<rootDir>/src/__mocks__/styleMock.js",
     ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>" }),
   },
   globals: {
