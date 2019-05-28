b2aaf2be2079908c35b12d76e8b4075cc9b6df86
lightyen <lightyen0123@gmail.com>
2019-05-28 13:50:59 +0800

    test

--- src/components/Example/__test__/Example.test.tsx
+++ src/components/Example/__test__/Example.test.tsx
@@ -8,7 +8,7 @@ import { Example, ExampleFC } from "../Example"
 test("Test my button", () => {
     const button0 = shallow(<Example content="hehehe" />)
     expect(button0.text()).toBe("hehehe")
-
     const button1 = shallow(<ExampleFC onClick={() => {}} />)
+    button1.simulate("click")
     expect(button1.text()).toBe("helloworld")
 })
