38ffd609c8b159b543faa84ea2b96789b6959db0
lightyen <lightyen0123@gmail.com>
2019-07-13 13:10:28 +0800

    test webfontloader

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,19 @@ import { render } from "react-dom"
 import "./utils/axios.interceptor"
 import App from "~/App"
 import { unregister } from "./serviceWorker"
-render(<App />, document.getElementById("root"))
+
+import WebFontLoader from "webfontloader"
+
+WebFontLoader.load({
+    classes: false,
+    custom: {
+        families: ["RootFont"],
+        urls: ["/assets/fonts/YaHei-Consolas.ttf"],
+        testStrings: {
+            RootFont: "Text 預載文字字型",
+        },
+    },
+    active: () => render(<App />, document.getElementById("root")),
+})
+
 unregister()
