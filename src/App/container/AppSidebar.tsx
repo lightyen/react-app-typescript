20ceeba3ff6612710a812952bb93b1b3c9c7335d
lightyen <lightyen0123@gmail.com>
2019-07-15 15:04:52 +0800

    update dropdown menu

--- src/App/container/AppSidebar.tsx
+++ src/App/container/AppSidebar.tsx
@@ -23,6 +23,7 @@ const NavDropdown = styled.ul.attrs<{ open: boolean }>(props => ({ className: "n
     overflow: hidden;
     ${props => (props.open ? "max-height: 1500px; opacity: 1.0;" : "max-height: 0; opacity: 0;")}
     transition: max-height 0.3s ease-in-out, opacity 0.5s ease-in-out;
+    position: relative;
 `
 
 const NavItem = styled.li.attrs(props => ({ className: "nav-item" }))``
@@ -44,30 +45,43 @@ const AppNavLink = styled(NavLink).attrs(props => ({ className: classnames("nav-
     }
 `
 
-const NavDropdownItem: React.FC<{ name: React.ReactNode; items: NavConfigItem[] }> = ({ name, items }) => {
+const NavDropdownItem: React.FC<{ name: React.ReactNode; items: NavConfigItem[]; className?: string }> = ({
+    name,
+    items,
+    className,
+}) => {
     const [open, setOpen] = useState(false)
     return (
         <li className="nav-item">
-            <NavDropdownItemLink onClick={() => setOpen(!open)}>
-                <span>{name}</span>
+            <NavDropdownItemLink onClick={() => setOpen(!open)} className={className}>
+                {name}
                 <NavDropdownCaret open={open}>
                     <i className="fas fa-caret-down" />
                 </NavDropdownCaret>
             </NavDropdownItemLink>
             <NavDropdown open={open}>
-                {items.map((item, i) => (
-                    <NavItem key={i} style={{ position: "relative", paddingLeft: "0.75rem" }}>
-                        <AppNavLink to={item.path} exact={item.exact} activeClassName="active">
-                            {item.name}
-                        </AppNavLink>
-                    </NavItem>
-                ))}
+                {items.map((item, i) => {
+                    switch (item.type) {
+                        case "normal":
+                            return (
+                                <NavItem key={i} style={{ position: "relative", paddingLeft: "0.75rem" }}>
+                                    <AppNavLink to={item.path} exact={item.exact} activeClassName="active">
+                                        {item.name}
+                                    </AppNavLink>
+                                </NavItem>
+                            )
+                        case "dropdown":
+                            return <NavDropdownItem key={i} name={item.name} items={item.items} />
+                        case "divider":
+                            return <NavDividerItem key={i} />
+                    }
+                })}
             </NavDropdown>
         </li>
     )
 }
 
-const NavDropdownItemLink = styled.a.attrs(props => ({ className: "nav-link" }))`
+const NavDropdownItemLink = styled.a.attrs(props => ({ className: classnames("nav-link", props.className) }))`
     outline: none;
     color: #f9f9f9;
     transition: background 0.2s, color 0.2s;
@@ -92,27 +106,29 @@ const NavDropdownCaret = styled.div<{ open: boolean }>`
     ${props => (props.open ? "" : "transform: rotate(90deg);")}
 `
 
+const NavDividerItem = styled.li`
+    width: 100%;
+    height: 0.5rem;
+    background: #242e40;
+`
+
 const AppSidebar: React.FC<RouteComponentProps> = () => {
     return (
         <Nav>
-            {navConfig.map((item, index) => {
-                if (item.items) {
-                    return (
-                        <NavDropdownItem key={index} name={"Dropdown"} items={item.items} />
-                        // <NavItem key={index}>
-                        //     <AppNavLink to={item.path} exact={item.exact} activeClassName="active">
-                        //         {item.name}
-                        //     </AppNavLink>
-                        // </NavItem>
-                    )
-                } else {
-                    return (
-                        <NavItem key={index}>
-                            <AppNavLink to={item.path} exact={item.exact} activeClassName="active">
-                                {item.name}
-                            </AppNavLink>
-                        </NavItem>
-                    )
+            {navConfig.map((item, i) => {
+                switch (item.type) {
+                    case "normal":
+                        return (
+                            <NavItem key={i}>
+                                <AppNavLink to={item.path} exact={item.exact} activeClassName="active">
+                                    {item.name}
+                                </AppNavLink>
+                            </NavItem>
+                        )
+                    case "dropdown":
+                        return <NavDropdownItem key={i} name={item.name} items={item.items} />
+                    case "divider":
+                        return <NavDividerItem key={i} />
                 }
             })}
         </Nav>
