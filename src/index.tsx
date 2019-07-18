b5fe5f169902f8c047384c2018cded3c37c74417
lightyen <lightyen0123@gmail.com>
2019-07-18 09:30:41 +0800

    change fonts

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ WebFontLoader.load({
     classes: false,
     custom: {
         families: ["RootFont"],
-        urls: [process.env.PUBLIC_URL + "/static/css/fonts.18749642.css"],
+        urls: [process.env.PUBLIC_URL + "/static/css/fonts.4b17b82b.css"],
         testStrings: {
             RootFont: "Text 預載文字字型",
         },
