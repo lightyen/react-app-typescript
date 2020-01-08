919103998ecb37a09bb21b92b6786b2b4e3d6085
lightyen <lightyen0123@gmail.com>
2020-01-08 15:48:07 +0800

    webpack: disable contentBase

--- .webpack/webpack.dev.js
+++ .webpack/webpack.dev.js
@@ -45,7 +45,7 @@ const config = {
             cachedAssets: true,
             warnings: true,
         },
-        contentBase: path.join(process.cwd(), "public"),
+        contentBase: false,
         // NOTE: 針對 createBrowserHistory, historyApiFallback 需要設定為 true, 且在實際應用中要後端支持。
         historyApiFallback: {
             rewrites: [
